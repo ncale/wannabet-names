81bec104e685c9fbebd5a89cc0f308f8fb8c2e30
Nick Brodeur <106794061+ncale@users.noreply.github.com>
2024-08-22 19:01:59 -0500

    feat: Add 404 page for not found routes

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import { H1 } from "@/components/headings";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <main className="max-w-xl mx-auto pt-24 space-y-2">
+      <H1>Page wasn&apos;t found</H1>
+      <p>Could not find requested resource</p>
+      <Link href="/" className="text-blue-500 underline block">
+        Return Home
+      </Link>
+    </main>
+  );
+}
