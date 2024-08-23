f51d06f106e90c965af81f616487392c0da76f85
Nick Brodeur <106794061+ncale@users.noreply.github.com>
2024-08-23 11:15:37 -0500

    style: update applied tw colors so both darkmode and light mode can work if/when applied

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-background">
         <Providers>
           <div className="px-4">
             <Header />
--- src/components/sections/landing.tsx
+++ src/components/sections/landing.tsx
@@ -3,12 +3,12 @@ import { Button } from "../ui/button";
 
 export default function LandingSection() {
   return (
-    <section className="bg-white dark:bg-gray-900 max-w-screen-lg mx-auto py-8 px-4 text-center lg:py-16 lg:px-12">
-      <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+    <section className="max-w-screen-lg mx-auto py-8 px-4 text-center lg:py-16 lg:px-12">
+      <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-foreground md:text-5xl lg:text-6xl">
         Claim free subdomains of{" "}
         <span className="text-primary">wannabet.eth</span>
       </h1>
-      <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+      <p className="mb-8 text-lg font-normal text-muted-foreground lg:text-xl sm:px-16 xl:px-48">
         Yep. Free. Just connect your wallet and claim.
       </p>
       <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
